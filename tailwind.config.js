396b3ee9a9880911d66d72242f38002b16a7f6b1
lakshmanan5678 <114216002+lakshmanan5678@users.noreply.github.com>
2025-05-19 19:28:47 +0530

    Add files via upload
    Initial portfolio website commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+      colors: {
+        primary: {
+          50: 'rgb(239, 246, 255)',
+          100: 'rgb(219, 234, 254)',
+          200: 'rgb(191, 219, 254)',
+          300: 'rgb(147, 197, 253)',
+          400: 'rgb(96, 165, 250)',
+          500: 'rgb(59, 130, 246)',
+          600: 'rgb(37, 99, 235)',
+          700: 'rgb(29, 78, 216)',
+          800: 'rgb(30, 64, 175)',
+          900: 'rgb(30, 58, 138)',
+        },
+      },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+      },
+      transitionProperty: {
+        'height': 'height',
+      },
+    },
+  },
+  plugins: [],
+};
